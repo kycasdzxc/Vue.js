d7a9cf44f8a16100436be639df00763a15176d1a
Yermi <kycasdzxc@naver.com>
2023-10-20 17:18:58 +0900

    ref -> refs 변경

--- vue-webgame/4.반응속도체크/ResponseCheck.vue
+++ vue-webgame/4.반응속도체크/ResponseCheck.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <div id="screen" :class="state" @click="onClickScreen()">{{message}}</div>
-        <div>
-            <div>평균 시간: {{result.reduce((a, c) => a + c, 0) / result.length || 0}}ms</div>
+        <template v-if="result.length">
+            <div>평균 시간: {{average}}ms</div>
             <button @click="onReset">리셋</button>
-        </div>
+        </template>
     </div>
 </template>
 
@@ -18,6 +18,11 @@
                 result: [],
                 state: 'waiting',
                 message: '클릭해서 시작하세요.'
+            }
+        },
+        computed: {
+            average() {
+                return this.result.reduce((a, c) => a + c, 0) / this.result.length || 0;
             }
         },
         methods: {
