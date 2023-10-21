f74b1ddb780b176a84d6a77a8678a5b9bd3ab703
Yermi <kycasdzxc@naver.com>
2023-10-21 13:34:01 +0900

    webgame : 로또

--- vue-webgame/6.로또/LottoGenerator.vue
+++ vue-webgame/6.로또/LottoGenerator.vue
@@ -1,115 +1,91 @@
 <template>
     <div>
-        <div id="computer" :style="computedStyleObject"></div>
-        <div>
-            <button @click="onClickButton('바위')">바위</button>
-            <button @click="onClickButton('가위')">가위</button>
-            <button @click="onClickButton('보')">보</button>
+        <div>당첨 숫자</div>
+        <div id="결과창">
+            <lotto-ball v-for="ball in winBalls" :key="ball" :number="ball"></lotto-ball>
         </div>
-        <div>{{result}}</div>
-        <div>현재 {{score}}점</div>
+        <div>보너스</div>
+        <lotto-ball v-if="bonus" :number="bonus"></lotto-ball>
+        <button v-if="redo" @click="onClickRedo">한 번 더!</button>
     </div>
 </template>
 
 <script>
-    const rspCoords = {
-        바위: '0',
-        가위: '-142px',
-        보 : '-284px',
-    };
+    import LottoBall from './LottoBall';
 
-    const scores = {
-        가위: 1,
-        바위: 0,
-        보: -1,
-    };
+    function getWinNumbers() {
+        console.log('getWinNumbers');
+        const candidate = Array(45).fill().map((v, i) => i + 1);
+        const shuffle = [];
+        while (candidate.length > 0) {
+        shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+        }
+        const bonusNumber = shuffle[shuffle.length - 1];
+        const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+        return [...winNumbers, bonusNumber];
+    } 
 
-    const computerChoice = (imgCoord) => {
-        return Object.entries(rspCoords).find(function(v) {
-            return v[1] === imgCoord;
-        })[0];
-    };
-
-    let interval = null;
+    const timeouts = [];
     export default {
+        components: {
+            LottoBall // 'lotto-ball': LottoBall
+        },
         data() {
             return {
-                imgCoord: rspCoords.바위,
-                result: '',
-                score: 0,
+                winNubmers: getWinNumbers(),
+                winBalls: [],
+                bonus: null,
+                redo: false,
             };
         },
         computed: {
-            computedStyleObject() {
-                return {
-                    background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`,
-                };
-            }
+            
         },
         methods: {
-            changeHand() {
-                interval = setInterval(() => {
-                if (this.imgCoord === rspCoords.바위) {
-                    this.imgCoord = rspCoords.가위;
-                } else if (this.imgCoord === rspCoords.가위) {
-                    this.imgCoord = rspCoords.보;
-                } else if (this.imgCoord === rspCoords.보) {
-                    this.imgCoord = rspCoords.바위;
-                }
-            }, 100);
+            onClickRedo() {
+                this.winNubmers = getWinNumbers();
+                this.winBalls = [];
+                this.bonus = null;
+                this.redo = false;
+                this.showBalls();
             },
-            onClickButton(choice) {
-                clearInterval(interval);
-                const myScore = scores[choice];
-                const cpuScore = scores[computerChoice(this.imgCoord)];
-                const diff = myScore - cpuScore;
-                if (diff === 0) {
-                    this.result = '비겼습니다.';
-                } else if ([-1, 2].includes(diff)) {
-                    this.result = '이겼습니다.';
-                    this.score += 1;
-                } else {
-                    this.result = '졌습니다.';
-                    this.score -= 1;
+            showBalls() {
+                for (let i = 0 ; i < this.winNubmers.length - 1 ; i++) {
+                    timeouts[i] = setTimeout(() => {
+                        this.winBalls.push(this.winNubmers[i]);
+                    }, (i + 1) * 1000);
                 }
-                setTimeout(() => {
-                    this.changeHand();
-                }, 1000);
-            }
-        },
-        beforeCreate() {
-            console.log('beforeCreate');
-        },
-        created() {
-            console.log('created');
-        },
-        beforeMount() {
-            console.log('beforeMount');
+                timeouts[6] = setTimeout(() => {
+                    this.bonus = this.winNubmers[6];
+                    this.redo = true;
+                }, 7000);
+            },
         },
         mounted() {
-            console.log('mounted');
-            this.changeHand();
-        },
-        beforeUpdate() {
-            console.log('beforeUpdate');
-        },
-        updated() {
-            console.log('updated');
+            this.showBalls();
         },
         beforeDestroy() {
-            console.log('beforeDestroy');
-            clearInterval(interval);
+            timeouts.forEach((t) => {
+                clearTimeout(t);
+            });
         },
-        destroyed() {
-            console.log('destroyed');
+        watch: { // 왠만하면 안쓰는 게 좋다.
+            // bonus(value, oldValue) {
+            //     console.log(value, oldValue);
+            //     if(value === null) {
+            //         this.showBalls();
+            //     }
+            // }
+            // winBalls(value, oldValue) {
+            //     console.log(value, oldValue);
+            //     if (value.length === 0) {
+            //         this.showBalls();
+            //     }
+            // }
         }
     }
 </script>
 
 <style scoped>
-  #computer {
-    width: 142px;
-    height: 200px;
-    background-position: 0 0;
-  }
+
 </style>
